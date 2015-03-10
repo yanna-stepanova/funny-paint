a4b5c8f185179b242ca98f3cb4e37531d2f8ebc7
yanna.stepanova@gmail.com <yanna.stepanova@gmail.com@9915651b-85b3-ac7a-1738-a8ee6d2e18a7>
2015-03-10 20:27:04 +0000

    Implemented main function; corrected project file;

--- core/headers/cr_mainwindow.h
+++ core/headers/cr_mainwindow.h
@@ -3,16 +3,23 @@
 
 #include <QMainWindow>
 
+namespace Ui {
+class MainWindow;
+}
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    virtual ~MainWindow();
 
 signals:
 
 public slots:
+
+private:
+    Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
--- core/sources/cr_mainwindow.cpp
+++ core/sources/cr_mainwindow.cpp
@@ -1,12 +1,15 @@
-#include "mainwindow.h"
+#include "headers/cr_mainwindow.h"
+#include "ui_cr_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
 {
-
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-
+  delete ui;
 }
 
